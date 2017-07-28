37f7c12037fabc05fb2bd85da5ee8a70b5102afd
SShellie <seanshellie@gmail.com>
2017-07-28 10:58:40 -0400

    first commit

---
+++ starter_files/blackjack.rb
@@ -0,0 +1,9 @@
+puts "TODO Implement the game of blackjack."
+require_relative 'card'
+require_relative 'deck'
+require_relative 'player'
+
+money = 100
+while money > 0
+
+end
---
+++ starter_files/player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_accessor :hand, :hand_value, :ace_count, :money
+  def initialize
+      @hand = []
+      @hand_value = 0
+      @money = 100
+      @ace_count = ace_count
+
+end
